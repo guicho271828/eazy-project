0e77f6f924468c8188a5663ea460ab8c31fa64da
Masataro Asai <guicho2.71828@gmail.com>
2014-11-22 13:44:51 +0900

    after 09f8490, added tests to simulate the menu

---
+++ t/create.lisp
@@ -0,0 +1,57 @@
+(in-package :eazy-project.test)
+(in-suite :eazy-project)
+
+
+(defvar *data* nil)
+(defun wrap (fn &optional (*data* *data*))
+  (handler-bind
+      ((ask (lambda (c)
+              (declare (ignorable c))
+              (destructuring-bind (menu-name . query) (pop *data*)
+                (format t "~&Current Menu: ~a" *current-menu*)
+                (format t "~&Simulating menu op: ~a" menu-name)
+                (format t "~&Available restarts: ~&~:{~30@<~s~> = ~s~%~}"
+                        (mapcar (lambda (r)
+                                  (list (restart-name r) r))
+                                (compute-restarts c)))
+                (let ((r (find-restart menu-name c)))
+                  (assert r)
+                  (format t "~&Invoking restart: ~a" r)
+                  (wrap (lambda () (apply #'invoke-restart r query))))))))
+    (funcall fn)))
+
+
+
+
+(defun simulate-menu-selection (alist)
+  (wrap (lambda () (launch-menu)) alist))
+
+(defvar *projects*
+  (asdf:system-relative-pathname
+   :eazy-project.test
+   "t/test-projects/"))
+
+(test create
+  (let ((*config* (copy-seq *config*)))
+    (setf (getf *config* :LOCAL-REPOSITORY) *projects*)
+    (shell-command (format nil "rm -rf ~a" *projects*))
+    (finishes
+      (simulate-menu-selection
+       `((eazy-project::create-project)
+         (:name "test")
+         (eazy-project::create))))
+    (is
+     (probe-file (merge-pathnames "test/test.asd" *projects*)))
+    (finishes
+      (load
+       (merge-pathnames "test/test.asd" *projects*))
+      (load
+       (merge-pathnames "test/test.test.asd" *projects*)))
+    (is-true
+     (asdf:load-system :test))
+    (is-true
+     (asdf:load-system :test.test))))
+
+
+    
+     
