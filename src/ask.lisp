ff895515791c511d5967d57fafd0ee38da5bb944
Masataro Asai <guicho2.71828@gmail.com>
2014-11-23 11:00:57 +0900

    show the restart menu in the debug output when swank is not used

--- src/ask.lisp
+++ src/ask.lisp
@@ -13,9 +13,16 @@
          :format-control
          (concatenate 'string 
                       "(You are now in menu ~a.)~%"
-                      format-control)
+                      format-control
+                      #-(or swank sbcl)
+                      "~2&Available Actions:~%~:{~20@<~a: [~a]~> ~a~%~}")
          :format-arguments
-         (cons *current-menu* args)))
+         `(,*current-menu*
+           ,@args
+           #-(or swank sbcl)
+           ,(let ((rs (compute-restarts)))
+                 (mapcar (lambda (i r) (list i (restart-name r) r))
+                         (iota (length rs)) rs)))))
 
 (defun askp (c)
   (typep c 'ask))
